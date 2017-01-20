6db6c52f62214cbeb03c2c47c5e5e05820451d30
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2017-01-20 18:49:12 +0900

    fix readme and meta.

--- wp-sitemanager-mypace-custom-title-tag-adapter.php
+++ wp-sitemanager-mypace-custom-title-tag-adapter.php
@@ -2,10 +2,10 @@
 
 /**
  * Plugin Name:     WP SiteManager Mypace Custom Title Tag Adapter
- * Plugin URI:      https://github.com/torounit/wp-sitemanager-mypace-custom-title-tag-adapter
- * Description:     WP SiteManager Mypace Custom Title Tag Adapter
- * Author:          Team Lens
- * Author URI:      https://www.m-g-n.me/team-lens/
+ * Plugin URI:      https://github.com/LENS-mgn/wp-sitemanager-mypace-custom-title-tag-adapter
+ * Description:     Mypace Custom Title Tag support for WP SiteManager.
+ * Author:          megane9988, Toro_Unit
+ * Author URI:      https://www.m-g-n.me/
  * Text Domain:     wp-sitemanager-mypace-custom-title-tag-adapter
  * Domain Path:     /languages
  * Version:         0.1.0
@@ -15,6 +15,9 @@
 
 require 'classes/class-wp-sitemanager-mypace-custom-title-tag-adapter.php';
 
+/**
+ * Entry Point.
+ */
 function wp_sitemanager_mypace_custom_title_tag_adapter() {
 
 	if ( class_exists( 'Mypace_Custom_Title_Tag' ) and class_exists( 'WP_SiteManager' ) ) {
